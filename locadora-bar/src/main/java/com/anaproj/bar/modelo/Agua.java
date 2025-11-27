b533f85f961e9498a535d97bb211f58e14f8fddb
jpsousas <jpsousa@inf.ufsm.br>
2025-11-27 00:25:58 -0300

    feat: adding business rules and toString methods

--- locadora-bar/src/main/java/com/anaproj/bar/modelo/Agua.java
+++ locadora-bar/src/main/java/com/anaproj/bar/modelo/Agua.java
@@ -1,13 +1,26 @@
 package com.anaproj.bar.modelo;
 
 public class Agua implements Bebida {
+    private static final String ID = "agua";
+    private static final String DESCRICAO = "Água Filtrada";
+    private static final double PRECO = 2.0;
+
+    public String getId() {
+        return ID;
+    }
+
     @Override
     public String getDescricao() {
-        return "Água";
+        return DESCRICAO;
     }
 
     @Override
     public double getPreco() {
-        return 2.0;
+        return PRECO;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Agua{id='%s', descricao='%s', preço=R$%.2f}", ID, DESCRICAO, PRECO);
     }
 }
